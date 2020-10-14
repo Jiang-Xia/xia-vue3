3f451a40598bde0d2b7d5e739d299864a4c57d48
JiangChuanJun <JiangChuanJun@deepaint.cn>
2020-10-14 16:32:15 +0800

    基本目录结构

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import { createApp } from 'vue'
+import Vue, { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
+import router from './router/index'
+import store from './store'
+// import './import'
+
+/*
+  解决点解当前路由报错
+*/
+import Router from 'vue-router'
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location, onResolve, onReject) {
+  if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject)
+  return originalPush.call(this, location).catch(err => err)
+}
+
+Vue.config.productionTip = true
 createApp(App).use(store).use(router).mount('#app')
